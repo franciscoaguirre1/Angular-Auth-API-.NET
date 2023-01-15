8a9d485b02e356211ba7e9df5392293a2c0eb054
Francisco Aguirre <franmaguirre@gmail.com>
2023-01-15 12:35:39 -0300

    angular routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+
+
 
 @NgModule({
   declarations: [
@@ -14,7 +17,8 @@ import { SignupComponent } from './components/signup/signup.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
@@ -9,17 +11,39 @@ export class LoginComponent implements OnInit {
 
   type:string = "password";
   isText : boolean = false;
-  eyeIcon: string = "fa fa-eye slash"
+  eyeIcon: string = "fa fa-eye slash";
+  loginForm! : FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
   }
 
   hideShowPass() {
     this.isText = !this.isText;
     this.isText? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
     this.isText? this.type = "text" : this.type = "password"
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    } else {
+      console.log("Form is not valid");
+    }
+
+  }
+
+  private validateAllFormsFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach(element => {
+      const control = formGroup.get;
+      if(control instanceof FormControl) {
+        control.markAsDirty({onlySelf:true});      } // me quede en el min 20 de la parte 2 de https://www.youtube.com/watch?v=p9ScsROLjdI&t=835s
+    });
   }
 
 }
